f9a3aaa8f288daf519761856e6e8a33d938510b4
Serhii Amelin <amserega90@gmail.com>
2023-04-03 16:59:06 +0300

    add files

---
+++ ArrayProducts.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class ArrayProducts
+    {
+        private static string[] names;
+        private static double[] price;
+        public const int arrlength = 5;
+
+        
+        public ArrayProducts()
+        {
+            names = new string[arrlength] { "Батон горчичиный", "Пицца Буфет", "Яблоко Вкусное", "Яйцо С1", "Масло Маргарин" };
+            price = new double[arrlength] { 21.20, 75.10, 300.10, 12.99, 55.10 };
+        }
+        public static string[] ArrayListNames()
+        {
+            return names;
+        }
+        public static double[] ArrayListPrice()
+        {
+            return price;
+        }
+        public static void ArrayDisplay()
+        {
+            int j = 1;
+            for (int i = 0; i < arrlength; i++)
+            {
+                Console.WriteLine($"{j}. {names[i]} {price[i]:C}");
+                j++;
+            }
+        }
+    }
+}
---
+++ Start.cs
@@ -0,0 +1,53 @@
+﻿using ShoppingDay.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Start
+    {
+        public static void Run()
+        {
+            Console.WriteLine("Добро пожаловать в магазин!");
+
+            Console.WriteLine("Список наших товаров: ");
+            ArrayProducts arr = new ArrayProducts();
+
+
+            while (true)
+            {
+                ArrayProducts.ArrayDisplay();
+
+                Console.Write("Выберите товар: ");
+
+                while (true)
+                {
+                    string? text = Console.ReadLine();
+                    if (int.TryParse(text, out int number))
+                    {
+                        if (number > 0 && number <= ArrayProducts.arrlength)
+                        {
+                            Cart.AddToCart(ArrayProducts.ArrayListNames()[number - 1], ArrayProducts.ArrayListPrice()[number - 1]);
+                            break;
+                        }
+                        Console.WriteLine("Не удалось распознать число, попробуйте еще раз.");
+                    }
+                }
+
+                Console.WriteLine("Чтобы перейти к оформлению заказа нажмите \"N\"");
+                Console.WriteLine("Чтобы продолжить покупки - нажмите любую другую клавишу");
+                string? answer = Console.ReadLine();
+                if (!String.IsNullOrEmpty(answer) && answer.Equals("n", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    Console.WriteLine("Переходим в корзину для оформления заказа");
+                    break;
+                }
+            }
+            Cart.DisplayCart();
+            Order.OrderFinal();
+        }
+    }
+}
